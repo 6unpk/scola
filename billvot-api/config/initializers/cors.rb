966bc5070a4ba0d2c40c22ef48418941a57622b5
Junu Park <pkjoho95@gmail.com>
2026-02-07 02:53:12 +0900

    cors

--- billvot-api/config/initializers/cors.rb
+++ billvot-api/config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "http://127.0.0.1:5173"
+    origins "http://localhost:5173", "http://127.0.0.1:5173",
+            ENV.fetch("FRONTEND_URL", "https://billvot.vercel.app")
 
     resource "*",
       headers: :any,
